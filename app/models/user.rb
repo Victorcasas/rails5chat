1645cebb78b7521a66954832e643faf1886e0fa0
victorcasas <victor.casas.sm@gmail.com>
2017-01-20 09:36:08 +0000

    viernes3st

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
   has_many :chat_rooms, dependent: :destroy
   has_many :messages, dependent: :destroy
 
