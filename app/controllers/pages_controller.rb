de9963cccf2c9421edc47cc3040cbda21a232e28
Víctor Casas <victor.casas.sm@gmail.com>
2017-02-03 13:25:05 +0100

    m

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
-class PagesController < HighVoltage::PagesController  
-  
-  
+class PagesController < ApplicationController
+  includes HighVoltage::StaticPage 
+  skip_before_action :authenticate_user!
 
 end
