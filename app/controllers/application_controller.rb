63c3dc6eb0acfe8c800c67f12bb20beeb3ec111e
Víctor Casas <victor.casas.sm@gmail.com>
2017-02-03 19:35:57 +0100

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, :except => [:index]
+  before_action :authenticate_user!, :except => [:show]
   
 
 end
