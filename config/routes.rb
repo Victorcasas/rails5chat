43cd7152448b55d53f3a9c762569ddda684f25e6
Víctor Casas <victor.casas.sm@gmail.com>
2017-02-04 13:54:29 +0100

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, path: '', path_names: { sign_in: '', sign_out: 'logout'}
 
   resources :chat_rooms, only: [:new, :create, :show, :index]
   
